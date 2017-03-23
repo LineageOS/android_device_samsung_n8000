49e2526330a7fbd8d8bb9bfdb989b7893a5a7b3b
RGIB <gibellini.roberto@gmail.com>
2017-03-23 21:15:43 +0100

    n8000 : it's LineageOS time + cleanup
    Change-Id: I6c2ad1691d6a2e703d6faba5c0f7aff9c6a9cbfd

--- cm.mk
+++ cm.mk
@@ -1,11 +1,5 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := n8000
-
-# Boot animation
-TARGET_BOOTANIMATION_NAME := horizontal-1200
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -21,4 +15,8 @@ PRODUCT_MODEL := GT-N8000
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-N8000 TARGET_DEVICE=GT-N8000 BUILD_FINGERPRINT="samsung/p4noterfxx/p4noterf:4.0.4/IMM76D/N8000XWALG9:user/release-keys" PRIVATE_BUILD_DESC="p4noterfxx-user 4.0.4 IMM76D N8000XWALG9 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=GT-N8000 \
+	TARGET_DEVICE=GT-N8000 \
+	BUILD_FINGERPRINT="samsung/p4noterfxx/p4noterf:4.0.4/IMM76D/N8000XWALG9:user/release-keys" \
+	PRIVATE_BUILD_DESC="p4noterfxx-user 4.0.4 IMM76D N8000XWALG9 release-keys"
